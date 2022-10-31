bf269c277d95d23adada4cf071e741224e0bc040
Colleen Lemak <clemak11@gmail.com>
2022-10-31 13:01:32 -0700

    header file

--- p5/buffer.h
+++ p5/buffer.h
@@ -1,7 +1,13 @@
-/* buffer.h*/
+/**
+ * Colleen Lemak
+ * Operating Systems Sect2
+ * Dr. Y. Zhang
+ * 2 November 2022
+ */
 
 typedef int buffer_item;
 #define BUFFER_SIZE 5
 
 int insert_item(buffer_item item);
 int remove_item(buffer_item *item);
+void initData();
